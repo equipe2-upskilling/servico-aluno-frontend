b1bf2991d1a877b8301dffd0fe1d6a6f5b127fcd
Raissa Arruda de Araujo <raissa.arruda@animaeducacao.com.br>
2023-07-18 00:18:45 -0300

    feat: :tada: Inicio e configuração do projeto

---
+++ servico-aluno-frontend/src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import Header from './components/header.vue'
+import User from './components/User.vue'
+</script>
+
+<template>
+  <Header />
+  <User />
+
+  <main></main>
+</template>
+
+<style scoped></style>
---
+++ servico-aluno-frontend/src/components/Header.vue
@@ -0,0 +1,17 @@
+<script setup>
+const nome = 'Raissa'
+</script>
+
+<template>
+  <div>
+    <h1>{{ nome }}</h1>
+  </div>
+</template>
+
+<style scoped>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+</style>
